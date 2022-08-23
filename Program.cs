be34ea193f630f294cd048576f3dd56a0ceefa24
Dennie Declercq <dennie@ddsoft.be>
2022-08-23 18:11:03 +0200

    main2 branch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DDControllers;
+﻿using DDControllers;
 
 var builder = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
